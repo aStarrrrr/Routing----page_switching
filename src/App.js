231df0dd581ad1a23e4e646f930f864b6bf33cd5
Abhinand Shaji <abhinandshaji369@gmail.com>
2025-01-25 12:24:45 +0530

    Page switching using if without using routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import About from './Container/About';
+import Profile from './Container/Profile';
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState('');
+  let component
+  if(state === 'profile'){
+    component = <Profile></Profile>
+  }
+  if(state === 'about'){
+    component = <About></About>
+  }
   return (
-    <h1>Hi</h1>
+    <div>
+      <button onClick={()=>setState('profile')}>Profile</button>
+      <button onClick={()=>setState('about')}>About</button>
+      {component}
+    </div>
   );
 }
 
---
+++ src/Container/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h4>Iam a computer science student</h4>
+    </div>
+  )
+}
+
+export default About
